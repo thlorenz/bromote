40cba006204cf4e5dd68ee579f43181ed2b35e76
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-03 16:03:19 -0400

    adapting interface to consume hash

--- index.js
+++ index.js
@@ -8,15 +8,17 @@ module.exports = function addRemotes(remotes, cb) {
   if (!remotes.length) return cb(null, []);
   
   var gens = [];
-  var tasks = remotes.map(function (r) {
-    return function (cb) { 
-      genRemote(r, function (err, gen) {
-        if (err) return cb(err);
-        gens.push(gen);
-        cb();
-      });
-    };
-  });
+  var tasks = Object.keys(remotes)
+    .map(function (k) {
+      var remote = remotes[k];
+      return function (cb) { 
+        genRemote(remote, function (err, gen) {
+          if (err) return cb(err);
+          gens.push(gen);
+          cb();
+        });
+      };
+    });
 
   tasks.push(function (err) {
     if (err) return cb(err);
