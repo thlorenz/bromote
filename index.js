ed759ca15a11b239ccf9893939c3920a974924e8
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-04 12:16:06 -0400

    cleaning loaders on every call to bromote

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 'use strict';
 
-var genRemote =  require('./lib/gen-remote');
-var xtend     =  require('xtend');
-var runnel    =  require('runnel');
+var cleanLoaders =  require('./lib/clean-loaders');
+var genRemote    =  require('./lib/gen-remote');
+var xtend        =  require('xtend');
+var runnel       =  require('runnel');
 
 module.exports = function addRemoteScripts(remote, cb) {
-  if (!remote) return cb(null, []);
 
-  var keys = Object.keys(remote); 
-  if (!keys.length) return cb(null, []);
-  
-  var gens = [];
+  cleanLoaders(function (err) {
+    if (err) return cb(err);  
+    if (!remote) return cb(null, []);
 
-  var tasks = keys 
-    .map(function (k) {
-      var rem = remote[k];
-      rem.key = k;
+    var keys = Object.keys(remote); 
+    if (!keys.length) return cb(null, []);
+    
+    var gens = [];
 
-      return function (cb) { 
-        genRemote(rem, function (err, gen) {
-          if (err) return cb(err);
-          gens.push(gen);
-          cb();
-        });
-      };
+    var tasks = keys 
+      .map(function (k) {
+        var rem = remote[k];
+        rem.key = k;
 
+        return function (cb) { 
+          genRemote(rem, function (err, gen) {
+            if (err) return cb(err);
+            gens.push(gen);
+            cb();
+          });
+        };
+
+      });
+
+    tasks.push(function (err) {
+      if (err) return cb(err);
+      cb(null, gens);
     });
 
-  tasks.push(function (err) {
-    if (err) return cb(err);
-    cb(null, gens);
+    runnel(tasks);
   });
-
-  runnel(tasks);
 };
