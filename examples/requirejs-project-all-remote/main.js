f19025c98fd96902735e9c663753ff9014b3a30e
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-04 21:19:36 -0400

    more demos in requirejs example project that loads all third parties

--- examples/requirejs-project-all-remote/main.js
+++ examples/requirejs-project-all-remote/main.js
@@ -1,5 +1,3 @@
-console.log('loading main');
-
 var bromote = require('bromote');
 
 bromote.backbone(function (backbone) {
@@ -7,4 +5,10 @@ bromote.backbone(function (backbone) {
   console.log('backbone uses jquery version', backbone.$().jquery);  
   console.log('since backbone intialized properly, this means that underscore was added to window in time');
   console.log('window._.VERSION', window._.VERSION);
+
+  bromote.jquery(function ($) {
+    console.log('loading jquery again just pulls it from the bromote cache instead of making another request');
+    console.log('check your Network tab to confirm ;), also ... ');
+    console.log('$ === backbone.$ => ', $ === backbone.$);
+  });
 });
