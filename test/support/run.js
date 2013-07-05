976e5c46f1d9b91b551946b82d58806d0ea66576
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-05 11:48:59 -0400

    run is more configurable to allow running multiple tests

--- test/support/run.js
+++ test/support/run.js
@@ -5,17 +5,38 @@ var fs     =  require('fs')
   , path   =  require('path')
   , opener =  require('opener')
 
-module.exports = function (root) {
 
-  var bundle =  path.join(root, 'bundle.js')
-    , index  =  path.join(root, 'index.html')
-    , build  =  require(path.join(root, 'build'))
+function html(bundle, title) {
+  return [
+    '<!DOCTYPE html>'
+    , '<html>'
+    , '<head>'
+    , '<script src="' + bundle + '"></script>'
+    , '  <title>' + title + '</title>'
+    , '</head>'
+    , '<body>'
+    , ' <h3>' + title + '</h3>'
+    , '</body>'
+    , '</html>'
+    ].join('\n');
+}
+
+module.exports = function (root, opts, cb) {
+
+  opts = opts || {}
+  cb = cb || function () {}
+
+  var bundle =  path.join(root, opts.bundle || 'bundle.js')
+    , index  =  path.join(root, opts.html || 'index.html')
+    , build  =  opts.build || require(path.join(root, 'build'))
+    , title  =  opts.title || opts.html || 'bromote test'
+    ;
 
   build(true)
     .on('error', console.error)
     .on('end', function () {
-      fs.writeFileSync(index, '<script src="bundle.js"></script>', 'utf8');
-      opener(index)
+      fs.writeFileSync(index, html(opts.bundle || 'bundle.js', title), 'utf8');
+      opener(index, cb)
     })
     .pipe(fs.createWriteStream(bundle, 'utf8'));
 };
